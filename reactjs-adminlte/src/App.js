3e6dfc49872344724a273ecc1bccd4cb5637d289
akashraj98 <akashraj7713@gmail.com>
2020-11-24 20:59:34 +0530

    retailor ui

---
+++ reactjs-adminlte/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Menu from './components/Menu'
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Menu/>
+      <Content/>
+      <Footer/>
+    </div>
+  )
+}
